98f8bf04fa25dd7885c5dfb12a44522b7b71a14f
AndyCodez <andystudy4@gmail.com>
2016-06-18 12:39:49 +0300

    Write test for the static pages controller actions

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,23 +1,23 @@
 require 'test_helper'
 
-class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
-    get static_pages_home_url
+    get :home
     assert_response :success
   end
 
   test "should get about" do
-    get static_pages_about_url
+    get :about
     assert_response :success
   end
 
   test "should get help" do
-    get static_pages_help_url
+    get :help
     assert_response :success
   end
 
   test "should get contact" do
-    get static_pages_contact_url
+    get :contact
     assert_response :success
   end
 
