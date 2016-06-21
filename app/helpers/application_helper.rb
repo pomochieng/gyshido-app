3bde69c0da76e40cbca2b829afe85988ee924c35
pomochieng <pomochieng@gmail.com>
2016-06-21 16:11:59 +0300

    Creating Title Helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+	def complete_title(title="")
+		base_title = "Gyshido App"
+		if title.empty?
+			title= base_title
+		else
+			title = "#{title} | #{base_title}"
+		end
+	end
+
 end
